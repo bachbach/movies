9344ab2d0bc52b0c712c6a7a4655a228517da6de
Barbara Pietrzyk <b.pietrzyk90@gmail.com>
2019-08-13 18:18:25 +0200

    Style movie details screen

--- src/screens/Movie/Movie.js
+++ src/screens/Movie/Movie.js
@@ -1,23 +1,60 @@
 import React, { useEffect } from 'react'
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
+import './Movie.scss'
 
 const Movie = ({ movie, match, getMovie }) => {
   useEffect(() => {
     getMovie(match.params.id)
   }, [])
-  console.log(movie.postUrl)
+
+  const getActors = () => movie.actors.map(actor => (
+    <span
+      key={actor.imdbId}
+      className='movie__details__list'
+    >
+      {actor.name}
+    </span>)
+  )
+  const getGenres = () => movie.genres.map(genre => (
+    <span
+      key={genre}
+      className='movie__details__list'
+    >
+      {genre}
+    </span>)
+  )
+
   return (
-    <div>
+    <div className='movie'>
+      <Link to='/movies'>Back to list</Link>
+      <h1>{movie.title}</h1>
+      <div className='movie__details'>
       <div>
-        <div>Title:</div>
-        <div>{movie.title}</div>
-        <div>Director:</div>
-        <div>{movie.director}</div>
-        <div>Year:</div>
-        <div>{movie.year}</div>
-        <div>Metascore:</div>
-        <div>{movie.metascore}</div>
+        <div className='movie__details__item'>
+          <div className='label'>Director:</div>
+          <div className='value'>{movie.director}</div>
+        </div>
+        <div className='movie__details__item'>
+          <div className='label'>Year:</div>
+          <div className='value'>{movie.year}</div>
+        </div>
+        <div className='movie__details__item'>
+          <div className='label'>Metascore:</div>
+          <div className='value'>{movie.metascore}</div>
+        </div>
+        <div className='movie__details__item'>
+          <div className='label'>Actors: </div>
+          <div className='value'>{getActors()}</div>
+        </div>
+        <div className='movie__details__item'>
+          <div className='label'>Genres: </div>
+          <div className='value'>{getGenres()}</div>
+        </div>
+      </div>
+      <div>
         <img src={movie.posterUrl} alt="post url"/>
+      </div>
       </div>
     </div>
   )
@@ -25,7 +62,9 @@ const Movie = ({ movie, match, getMovie }) => {
 
 Movie.defaultProps = {
   movie: {
-    imdbId: ''
+    imdbId: '',
+    actors: [],
+    genres: []
   }
 }
 
